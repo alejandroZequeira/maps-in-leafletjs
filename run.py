d3196f8a866362cbc7868c4f286297409879951d
Alejandro de la Rosa Zequeira <65790899+alejandroZequeira@users.noreply.github.com>
2023-08-21 16:16:26 -0600

    implementacion del geojson

---
+++ run.py
@@ -0,0 +1,34 @@
+import json as simplejson
+import os
+from flask import Flask, request, jsonify, abort, render_template, redirect, url_for, session, escape
+
+import json
+
+
+# Config Flask
+app = Flask(__name__)
+app.secret_key = 'jupiter'
+app.config.update(
+    DEBUG=True,
+    JSON_SORT_KEYS=True
+)
+
+
+# Root
+@app.route('/')
+@app.route('/index')
+def main():
+    return render_template('map.html')
+
+
+#get user
+@app.route('/geojsonCP/<id>', methods=['GET'])
+def geojson(id):
+        with open("static/geojson/Municipios/"+id,"r") as j:
+             mydata=json.load(j)
+        return jsonify(mydata)
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=45000, debug=True, threaded=True)
--- static/main.js
+++ static/main.js
@@ -1,5 +1,6 @@
+var statesData={}
 var region=document.querySelector('#region')
-var map = L.map("map").setView([24.3635272, -99.9940824], 5)
+var map = L.map("map").setView([21.657428, -100.942384], 5)
 var popup = L.popup()
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -17,18 +18,31 @@ region.addEventListener("change",function(){
     console.log("entro a la funcion")
     console.log("valor del select",this.value)
     switch (this.value) {
+        case "0": {
+            map.flyTo([21.657428, -100.942384],5) 
+            break;
+        }
         case "1":{ 
             console.log("entro a la primera opcion")
+            geoJsonCP("24_SanLuisPotosi.json")
             map.flyTo([22.1498200,-100.9791600],8)
+            L.geoJson(statesData.features,{style:{color: "#7AE9EE"}}).addTo(map)
             break;
         }
         default:
-            console.log("no cambio")
+            map.flyTo([21.657428, -100.942384],5)
     }
 })
-const requesturl="geojson-master/mexicoHigh.json"
-const request=new XMLHttpRequest()
-request.open("get",requesturl)
-console.log(request.status)
-L.geoJson(statesData).addTo(map)
+
+function geoJsonCP(id){
+    fetch('/geojsonCP/'+id,{
+        "method":"GET",
+        "headers":{
+            "Content-Type":"application/json"
+        },
+    }).then(response => response.text())
+    .then(res=>{
+        statesData=JSON.parse(res)
+    });
+}
 map.on("click", onMapClick)
