e765017dfc8494577081f0f37e9e742d5cc927f6
alejandroZequeira <alejandrozequeirarosa@gmail.com>
2023-08-22 17:03:11 -0600

    modificacion los geojson

--- run.py
+++ run.py
@@ -3,13 +3,6 @@ import os
 from flask import Flask, request, jsonify, abort, render_template, redirect, url_for, session, escape
 
 import json
-data={    
-}    
-
-with open("static\geojson\mexicoHigh.json","r") as r:
-         data=json.load(r)
-
-print(data)
 
 # Config Flask
 app = Flask(__name__)
@@ -27,9 +20,21 @@ def main():
 
 
 #get user
-@app.route('/geojsonCP/<id>', methods=['GET'])
-def geojson(id):
+@app.route('/geojson/CP/<id>', methods=['GET'])
+def geojsonCP(id):
+        with open("static/geojson/CP/"+id,"r") as j:
+             mydata=json.load(j)
+        return jsonify(mydata)
+
+@app.route('/geojson/municipios/<id>', methods=['GET'])
+def geojsonM(id):
         with open("static/geojson/Municipios/"+id,"r") as j:
+             mydata=json.load(j)
+        return jsonify(mydata)
+
+@app.route('/estados', methods=['GET'])
+def states():
+        with open("static/geojson/estados.json","r") as j:
              mydata=json.load(j)
         return jsonify(mydata)
 
--- static/main.js
+++ static/main.js
@@ -1,19 +1,20 @@
 var statesData={}
-var region=document.querySelector('#region')
+var sates={}
+var CP={}
+var region=document.querySelector('#estado')
 var map = L.map("map").setView([21.657428, -100.942384], 5)
 var popup = L.popup()
+var info= L.control()
+var geojson
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     minZoom: 5,
     attribution: '© OpenStreetMap'
 }).addTo(map)
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("diste click "+e.latlng)
-        .openOn(map);
-}
+map.on("click", onMapClick)
+
 region.addEventListener("change",function(){
     console.log("entro a la funcion")
     console.log("valor del select",this.value)
@@ -22,9 +23,9 @@ region.addEventListener("change",function(){
             map.flyTo([21.657428, -100.942384],5) 
             break;
         }
-        case "1":{ 
+        case "24":{ 
             console.log("entro a la primera opcion")
-            geoJsonCP("24_SanLuisPotosi.json")
+            geoJsonM("24.json")
             map.flyTo([22.1498200,-100.9791600],8)
             break;
         }
@@ -34,7 +35,43 @@ region.addEventListener("change",function(){
 })
 
 function geoJsonCP(id){
-    fetch('/geojsonCP/'+id,{
+    fetch('/geojson/CP/'+id,{
+        "method":"GET",
+        "headers":{
+            "Content-Type":"application/json"
+        },
+    }).then(response => response.text())
+    .then(res=>{
+        CP=JSON.parse(res)
+    });
+}
+
+function geoJsonM(id){
+    fetch('/geojson/municipios/'+id,{
+        "method":"GET",
+        "headers":{
+            "Content-Type":"application/json"
+        },
+    }).then(response => response.text())
+    .then(res=>{
+        states=JSON.parse(res)
+        console.log(states)
+    });
+    geojson= L.geoJson(states,{
+        onEachFeature:onEachFeature
+    }).addTo(map)
+}
+
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("diste click "+e.latlng)
+        .openOn(map);
+}
+
+document.addEventListener("DOMContentLoaded",function(){
+    fetch('/estados',{
         "method":"GET",
         "headers":{
             "Content-Type":"application/json"
@@ -42,7 +79,58 @@ function geoJsonCP(id){
     }).then(response => response.text())
     .then(res=>{
         statesData=JSON.parse(res)
+        console.log(statesData)
+        geojson= L.geoJson(statesData,{
+            onEachFeature:onEachFeature
+        }).addTo(map)
+        for (data in statesData.features){
+            block='<option value='+statesData.features[data].properties.CODIGO+'>'+statesData.features[data].properties.ESTADO+'</option>'
+            region.innerHTML+=block
+           // console.log(statesData.features[data].properties.CODIGO)
+        }
     });
+})
+
+function highlightFeature(e){
+    var layer=e.target
+    layer.setStyle({
+        weight: 5,
+        color: "#666",
+        dasArray: '',
+        fillOpacity: 0.7
+    })
+
+    if(!L.Browser.ie && !L.Browser.opera && !L.Browser.edge){
+        layer.bringToFront()
+    }
+    info.update(layer.feature.properties)
 }
-L.geoJson(data.features,{style:{color: "#7AE9EE"}}).addTo(map)
-map.on("click", onMapClick)
+
+function resetHighlight(e){
+    geojson.resetStyle(e.target)
+    info.update()
+}
+
+function zoomToFeature(e){
+    map.fitBounds(e.target.getBounds())
+}
+
+function onEachFeature(feature, layer){
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    })
+}
+
+info.onAdd=function(map){
+    this._div=L.DomUtil.create("div","info")
+    this.update()
+    return this._div
+}
+
+info.update=function(props){
+    this._div.innerHTML='<h4>'+(props ? '<b>'+ props.ESTADO+'</b>':"Mexico")
+}
+
+info.addTo(map)
